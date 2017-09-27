d2e48c6564cd94132e3c1bbfe83cfb1d415dcd90
Nathan Rollins <nathan.rollins@asu.edu>
2017-09-27 16:07:30 -0700

    Drop page size 10 from list of options

--- app/containers/Catalog.jsx
+++ app/containers/Catalog.jsx
@@ -10,7 +10,7 @@ function Catalog(props: { posts: array, loading: boolean }) {
     defaultPageSize: `${wpr_object.gdr_default_page_size}`,
     showPagination: true,
     showPageSizeOptions: true,
-    pageSizeOptions: [10, 20, 50, 100],
+    pageSizeOptions: [20, 50, 100],
     showPageJump: true,
     collapseOnSortingChange: true,
     collapseOnPageChange: true,
